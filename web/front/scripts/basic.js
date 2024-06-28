82301fb614569e529b66a528344bb242ea33d1aa
Inal Kalmykov <i.kalmykov@innopolis.university>
2024-06-28 15:44:51 +0000

    Upload New File

---
+++ web/front/scripts/basic.js
@@ -0,0 +1,12 @@
+function loadPage(page) {
+    console.log('Loading page:', page);
+    const contentDiv = document.getElementById('content');
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            contentDiv.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open('GET', `pages/${page}`, true);
+    xhr.send();
+}
