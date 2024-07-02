3fa160435344b09a8251ae7cd089515e0e74fc06
Inal Kalmykov <i.kalmykov@innopolis.university>
2024-07-02 06:23:15 +0000

    Replace GroupsPage.jsx

--- web/front/src/components/GroupsPage.jsx
+++ web/front/src/components/GroupsPage.jsx
@@ -3,13 +3,20 @@ import './styles.css'; // Import the provided CSS file
 
 const GroupsPage = () => {
     const [groups, setGroups] = useState([]);
+    const [error, setError] = useState(null); // State for error message
 
     useEffect(() => {
         const fetchGroups = async () => {
-            const response = await fetch('/api/getGroups');
-            const data = await response.json();
-            setGroups(data);
-            console.log(data);
+            try {
+                const response = await fetch('/api/getGroups');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch groups');
+                }
+                const data = await response.json();
+                setGroups(data);
+            } catch (error) {
+                setError(error.message);
+            }
         };
 
         fetchGroups();
@@ -31,6 +38,11 @@ const GroupsPage = () => {
                     </nav>
                 </div>
             </div>
+            {error && (
+                <div className="error-box">
+                    <p>{error}</p>
+                </div>
+            )}
         </div>
     );
 };
