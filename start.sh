c8b890f43a25b329b17e0c19522ad36fa45cf31f
blxxdclxud <ramazanatzuf10@gmail.com>
2024-07-12 21:30:11 +0300

    deployment files fixed

--- start.sh
+++ start.sh
@@ -3,11 +3,12 @@
 # Debug: List files to verify server binary is present
 ls -al
 
-# Start the React frontend
-npm run start
-#npm run build
-#npm install -g serve
-#serve -s build -l 80
-
 # Start the Go server in the background
 ./server &
+
+# Start the React frontend
+#npm run start --prefix ./web/front
+cd ./web/front
+npm run build
+npm install -g serve
+serve -s build -l 80
